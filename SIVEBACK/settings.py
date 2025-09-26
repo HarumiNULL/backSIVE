74b2d77eeff6bb3793b780c0f87f9cb48d6b3288
Mr.Mostacho <133924865+MrMostacho04@users.noreply.github.com>
2025-09-26 01:07:50 -0500

    Agregado Django REST framework y configurada PostgreSQL database

--- SIVEBACK/settings.py
+++ SIVEBACK/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
+    "api",
 ]
 
 MIDDLEWARE = [
@@ -74,14 +76,19 @@ WSGI_APPLICATION = "SIVEBACK.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "sivedb",
+        "USER": "sive",
+        "PASSWORD": "sivePass",
+        "HOST": "localhost",
+        "PORT": "5433",
     }
 }
 
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
+AUTH_USER_MODEL = 'api.user'
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -102,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = "es-mx"
 
 TIME_ZONE = "UTC"
 
