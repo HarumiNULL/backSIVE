160626d86156d41fd13cbd6bdecf318b5ff67b7d
drago668 <dragoartworks@gmail.com>
2025-10-26 00:53:00 -0500

    arreglada la ruta para visualizar la carpeta media, y modificado el optical serializers para traer descriptionOp

--- api/serializers/optical_serializers.py
+++ api/serializers/optical_serializers.py
@@ -4,12 +4,12 @@ from rest_framework import serializers
 class OpticalCreateSerializers(serializers.ModelSerializer):
   class Meta:
     model = Optical
-    fields = ['nameOp', 'address', 'tel', 'city', 'email', 'logo', 'certCadecuacion', 'certDispensacion', 'latitud', 'longitud']
+    fields = ['id_optical', 'descriptionOp','nameOp', 'address', 'tel', 'city', 'email', 'logo', 'certCadecuacion', 'certDispensacion', 'latitud', 'longitud']
 
 class OpticalListSerializers(serializers.ModelSerializer):
   class Meta:
     model = Optical
-    fields = ['id_optical', 'nameOp', 'address', 'tel', 'city', 'email', 'logo', 'user', 'certCadecuacion', 'certDispensacion', 'latitud', 'longitud', 'view']
+    fields = ['id_optical', 'descriptionOp', 'nameOp', 'address', 'tel', 'city', 'email', 'logo', 'user', 'certCadecuacion', 'certDispensacion', 'latitud', 'longitud', 'view']
     read_only_fields = ['id_optical', 'view', 'user']
 
 class OpticalTopViewedSerializers(serializers.Serializer):
