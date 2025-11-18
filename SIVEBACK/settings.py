c604840d66ec4e7dfe20867e804e965104697acb
drago668 <dragoartworks@gmail.com>
2025-11-18 01:29:33 -0500

    agregado render.yaml y build.sh

--- SIVEBACK/settings.py
+++ SIVEBACK/settings.py
@@ -109,15 +109,11 @@ DATABASES = {
     "default": dj_database_url.config(default=os.getenv('DATABASE_URL'))
 }
 #CLOUDINARY_URL = os.getenv("CLOUDINARY_URL")
-DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+#DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-#MEDIA_URL = '/media/'
-#MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-CLOUDINARY_STORAGE = {
-    "CLOUD_NAME": os.getenv("CLOUDINARY_CLOUD_NAME"),
-    "API_KEY": os.getenv("CLOUDINARY_API_KEY"),
-    "API_SECRET": os.getenv("CLOUDINARY_API_SECRET"),
-}
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 
 
